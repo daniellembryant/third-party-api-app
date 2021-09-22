e9a83794c578e3561a6ad4d9b8eddd793f419692
daniellembryant <danbry08@yahoo.com>
2021-09-22 11:48:08 -0500

    Secures API key

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -2,7 +2,7 @@ class BreedsController < ApplicationController
 
   def index 
     user_search = params[:search]
-    response = HTTP.auth("<API KEY>").get("https://api.thecatapi.com/v1/breeds/search?q=#{user_search}")
+    response = HTTP.auth("Rails.application.credentials.cat_api").get("https://api.thecatapi.com/v1/breeds/search?q=#{user_search}")
     breeds = response.parse(:json)
     render json: breeds
   end
